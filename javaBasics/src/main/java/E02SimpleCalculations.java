ef982d285b11193644fae8738ff57cb6694ad784
AbdurakhmanV <146175665+AbdurakhmanV@users.noreply.github.com>
2024-02-03 16:34:07 +0100

    simple output of sum, difference, product and quotient;
    einfache Ausgabe von der Summe, Differenz, Produkt und Quotient;

--- javaBasics/src/main/java/E02SimpleCalculations.java
+++ javaBasics/src/main/java/E02SimpleCalculations.java
@@ -2,7 +2,7 @@ public class E02SimpleCalculations {
     public static void main(String[] args) {
         int x = 233;
         int y = 14;
-
+        System.out.println();
         System.out.println(x + "+" + y + " = " + (x + y));
         System.out.println(x + "-" + y + " = " + (x - y));
         System.out.println(x + "*" + y + " = " + (x * y));
