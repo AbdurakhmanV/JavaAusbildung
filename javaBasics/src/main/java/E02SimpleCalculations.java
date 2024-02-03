ddaebd4e722fb4fe5ecdbba1222dec3f32fbf25b
AbdurakhmanV <146175665+AbdurakhmanV@users.noreply.github.com>
2024-02-03 16:32:25 +0100

    simple output of sum, difference, product and quotient;
    einfache Ausgabe von der Summe, Differenz, Produkt und Quotient;

---
+++ javaBasics/src/main/java/E02SimpleCalculations.java
@@ -0,0 +1,19 @@
+public class E02SimpleCalculations {
+    public static void main(String[] args) {
+        int x = 233;
+        int y = 14;
+
+        System.out.println(x + "+" + y + " = " + (x + y));
+        System.out.println(x + "-" + y + " = " + (x - y));
+        System.out.println(x + "*" + y + " = " + (x * y));
+        System.out.printf(x + "/" + y + " = %.2f", ((double) x / (double) y));
+    }
+}
+
+//    Declare two numeric variables with arbitary values. Calculate their sum, difference,
+//    product and quotient and print the calculation with the result on the console with System.out.println/System.out.printf.
+//    Perform all calculations with whole numbers and fractional numbers.
+//    Bonus: Try to limit the decimal places of your calculations with the fractional numbers.
+//    Your output should look like this:
+//    138 + 235 = 373
+//    138 - 235 = -97
